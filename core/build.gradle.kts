eedf585c9acb4a271b4237555e0ddac3e7300911
SingTsunWONG <74446979+singtsun9941@users.noreply.github.com>
2024-08-11 17:03:45 +0800

    Update add ktor dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,3 +7,12 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.content.negotiation)
+}
