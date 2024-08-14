5d1636ec6ca37c3300c54f22192539b4b1bfe5cb
SingTsunWONG <74446979+singtsun9941@users.noreply.github.com>
2024-08-14 21:11:39 +0800

    Add NetworkError model

---
+++ core/src/main/java/com/stwcoding/baserepo/model/NetworkError.kt
@@ -0,0 +1,13 @@
+package com.stwcoding.baserepo.model
+
+sealed class NetworkError : Error("Network Error") {
+    class REQUEST_TIMEOUT : NetworkError()
+    class TOO_MANY_REQUEST : NetworkError()
+    class CONFLICT : NetworkError()
+    class NO_INTERNET : NetworkError()
+    class PAYLOAD_TOO_LARGE : NetworkError()
+    class UNAUTHORIZED : NetworkError()
+    data class SERVER_ERROR(val errorCode: String = "") : NetworkError()
+    class SERIALIZATION : NetworkError()
+    class UNKNOWN : NetworkError()
+}
