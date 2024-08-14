80212be8b4c732ae3511e621d8af870d5f6878d9
SingTsunWONG <74446979+singtsun9941@users.noreply.github.com>
2024-08-14 21:10:37 +0800

    Update set domain in createHttpClient

--- core/src/main/java/com/stwcoding/baserepo/createHttpClient.kt
+++ core/src/main/java/com/stwcoding/baserepo/createHttpClient.kt
@@ -3,13 +3,20 @@ package com.stwcoding.baserepo
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-fun createHttpClient(engine: HttpClientEngine): HttpClient {
+fun createHttpClient(
+    engine: HttpClientEngine,
+    domain: String,
+): HttpClient {
     return HttpClient(engine) {
+        defaultRequest {
+            url(domain)
+        }
         install(Logging) {
             level = LogLevel.ALL
         }
